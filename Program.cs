01bc1a64d1043e5dc209abc4e3e7dcb9690275c0
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-03 12:53:59 +0530

    A02 Completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter your lucky number");
+int Unum = Convert.ToInt32 (Console.ReadLine ());
+
+Random rnd = new ();
+int num = rnd.Next (0, 100);
+Console.WriteLine ("\nMatching result between your lucky number and computer generated number");// Guess the computer generated number
+
+while (num == Unum) {
+   Console.WriteLine ("\nHurray winner winner chicken dinner,your guess is corect");
+   break;
+}
+
+if (num < Unum) {
+   Console.WriteLine ("\nyour guess is high");
+} else {
+   Console.WriteLine ("\nyour guess is low");
+}
+//Console.WriteLine ("\nCorrect answer is  " + num);
