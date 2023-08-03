d4db9e114be523cdd044c0727302dbf99c5652d0
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-03 14:18:26 +0530

    Modified code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿Console.WriteLine ("Enter your lucky number");
-int Unum = Convert.ToInt32 (Console.ReadLine ());
+int unum = Convert.ToInt32 (Console.ReadLine ());
 
 Random rnd = new ();
 int num = rnd.Next (0, 100);
 Console.WriteLine ("\nMatching result between your lucky number and computer generated number");// Guess the computer generated number
 
-while (num == Unum) {
+while (num == unum) {
    Console.WriteLine ("\nHurray winner winner chicken dinner,your guess is corect");
    break;
 }
 
-if (num < Unum) {
+if (num < unum) 
    Console.WriteLine ("\nyour guess is high");
-} else {
+else 
    Console.WriteLine ("\nyour guess is low");
-}
+
 //Console.WriteLine ("\nCorrect answer is  " + num);
