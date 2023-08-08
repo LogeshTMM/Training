9166748fcb90634028e335c2deb28f16042308b0
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-08 10:32:16 +0530

    Working Code

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
 ﻿NewMethod ();
 
 static void NewMethod () {
-   Console.WriteLine ("Enter your lucky number");
-   int unum = Convert.ToInt32 (Console.ReadLine ());
+   Console.WriteLine ("Enter your name");
+   string uname = Console.ReadLine ();
 
+   Console.WriteLine ("\nEnter your lucky number");
    Random rnd = new ();
    int num = rnd.Next (0, 100);
-   Console.WriteLine ("\nMatching result between your lucky number and computer generated number");// Guess the computer generated number
+   //Console.WriteLine ("\nMatching result between your lucky number and computer generated number");// Guess the computer generated number
+   int j = 6;
+   for (int i = 0; i <= j; i++) {
+      
+      int unum = Convert.ToInt32 (Console.ReadLine ());
 
-   while (num == unum) {
-      Console.WriteLine ("\nHurray winner winner chicken dinner,your guess is corect");
-      break;
+      if (num == unum)
+         Console.WriteLine ($"Your are a genius {uname}","\n");
+   
+      else if (num < unum)
+         Console.WriteLine ($"your guess is high, Try again you have {j-i} chances left","\n");
+      else
+         Console.WriteLine ($"your guess is low, Try again you have {j-i} chances left","\n");
+
    }
-
-   if (num < unum)
-      Console.WriteLine ("\nyour guess is high");
-   else
-      Console.WriteLine ("\nyour guess is low");
 
    //Console.WriteLine ("\nCorrect answer is  " + num);
 }
