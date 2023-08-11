6c392f2e3843c4f628a40ab6063a4dc9c3d5331b
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-11 11:40:51 +0530

    Working code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿//Convert THE  GIVEN DECIMAL NUMBER TO BASE VALUE OF 16,8 AND 2
+internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Enter the decimal number ");
+         int user_no = Convert.ToInt32 (Console.ReadLine ());
+         Console.WriteLine ("\nwhich base value you want to convert\n\tif you want to convert Hexadecimal number then press 'h'\n\tif you want to convert Octal then press 'o'\n\tif you want to convert Binary numbers then press 'b'\n");
+         var user_respond_key = Console.ReadKey (true).KeyChar;
+         if (user_respond_key == 'h') Console.WriteLine (Convert.ToString (user_no, 16));
+         else if (user_respond_key == 'o') Console.WriteLine (Convert.ToString (user_no, 8));
+         else if (user_respond_key == 'b') Console.WriteLine (Convert.ToString (user_no, 2));
+         else Console.WriteLine ("Your press the wrong button");
+      }
+   }
