56d519b9dcc59d08d3c5613e18e6453973e271bf
LogeshTMM <logeshkumar.p.interns@metamation.com>
2023-08-14 10:08:46 +0530

    Working code v.2.0

--- T03/Program.cs
+++ T03/Program.cs
@@ -1,22 +1,22 @@
 ﻿//FIBONNACI SERIES
-   internal class Program {
-      static void Main () {
-         int[] fb = { 0, 1,1 };
-         int lk;
-         Console.WriteLine ("Enter the nth term of s fibonnaci series, to find the value and along with the series");
-         int n = Convert.ToInt32(Console.ReadLine());
-         for(int i =0;i<n;i++) { 
-            lk = fb[fb.Length-1] + fb[fb.Length-2];
-            fb = fb.Append(lk).ToArray();
-         }
-         Console.WriteLine ($"\nFibonnaci series of {n}th term is");
-         foreach(int j in fb) {
-            Console.Write (j);
-            Console.Write (" ");
-         }
-         Console.WriteLine ("\n");
-         Console.WriteLine ($"{n}th term value of fibonnaci series is\t");
-         Console.Write (fb[n]);
-         Console.WriteLine ("\n");
+internal class Program {
+   static void Main () {
+      int[] fb = { 0, 1, 1 };
+      int lk;
+      Console.WriteLine ("Enter the nth term of s fibonnaci series, to find the value and along with the series");
+      int n = Convert.ToInt32 (Console.ReadLine ());
+      for (int i = 0; i < n; i++) {
+         lk = fb[fb.Length - 1] + fb[fb.Length - 2];
+         fb = fb.Append (lk).ToArray ();if (i == n - 3) break;
       }
+      Console.WriteLine ($"\nFibonnaci series of {n}th term is");
+      foreach (int j in fb) {
+         Console.Write (j);
+         Console.Write (" ");
+      }
+      Console.WriteLine ("\n");
+      Console.WriteLine ($"{n}th term value of fibonnaci series is\t");
+      Console.Write (fb[n]);
+      Console.WriteLine ("\n");
    }
+}
